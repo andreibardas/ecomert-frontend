e2a0441bd07fc93fd06c5b0b2287a137516c3f5e
bardasandrei <bardas.andrei@yahoo.com>
2020-01-14 13:35:42 +0200

    Updates

---
+++ precache-manifest.fd7d29af1d7ce69b5a9a19ba8118b97a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b07859aa309da800c908105f638d34a",
+    "url": "/ecomert/index.html"
+  },
+  {
+    "revision": "22dc804a05dca7381112",
+    "url": "/ecomert/static/css/main.d5a05009.chunk.css"
+  },
+  {
+    "revision": "eefd008d13114a19d556",
+    "url": "/ecomert/static/js/2.69f449cc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ecomert/static/js/2.69f449cc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "22dc804a05dca7381112",
+    "url": "/ecomert/static/js/main.48a1e7ac.chunk.js"
+  },
+  {
+    "revision": "6844bc73d77dd2865d31",
+    "url": "/ecomert/static/js/runtime-main.e23c8ca9.js"
+  },
+  {
+    "revision": "5a848c29bc4bed4a4d83183fe1ac45b5",
+    "url": "/ecomert/static/media/alcool.5a848c29.jpg"
+  },
+  {
+    "revision": "eb5a225c279346e47382c01f83a1e985",
+    "url": "/ecomert/static/media/apa.eb5a225c.jpg"
+  },
+  {
+    "revision": "02607d01aa8f83d481777251b89d5b82",
+    "url": "/ecomert/static/media/bere.02607d01.jpg"
+  },
+  {
+    "revision": "e28a7906825e2dadba8ae0468dbf14f3",
+    "url": "/ecomert/static/media/cafea.e28a7906.jpg"
+  },
+  {
+    "revision": "5ec340f2addcf0f4943458b6ad076631",
+    "url": "/ecomert/static/media/dulciuri.5ec340f2.jpg"
+  },
+  {
+    "revision": "4391e867917d0ab1f0674ba070365daa",
+    "url": "/ecomert/static/media/snacks.4391e867.jpg"
+  },
+  {
+    "revision": "023c724a7c30abdc217dcfcbd703e420",
+    "url": "/ecomert/static/media/suc.023c724a.jpg"
+  },
+  {
+    "revision": "ac342ee6aea26de960d3efa1c82898bc",
+    "url": "/ecomert/static/media/tutun.ac342ee6.jpg"
+  }
+]);
