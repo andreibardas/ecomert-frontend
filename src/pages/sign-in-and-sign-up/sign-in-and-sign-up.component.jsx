583ec9ef1977c45c80d7f0e5a5c381b0935054bd
bardasandrei <bardas.andrei@yahoo.com>
2020-01-24 20:36:26 +0200

    adjusting scss for mobile

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -5,11 +5,13 @@ import SignIn from "../../components/sign-in/sign-in.component";
 import "./sign-in-and-sign-up.styles.scss";
 
 const SignInAndSignUpPage = () => (
+    <div>
     <div className="sign-in-and-sign-up">
         <ul className="tab-group">
-            <li className="tab-active"><a href="#signup">FA-TI CONT</a></li>
-            <li className="tab-inactive"><a href="#signin">INTRA IN CONT</a></li>
+            <li className="tab-active"><a href="#signin">INTRA IN CONT</a></li>
+            <li className="tab-inactive"><a href="#signup">FA-TI CONT</a></li>
         </ul>
+    </div>
         <SignIn/>
     </div>
 );
