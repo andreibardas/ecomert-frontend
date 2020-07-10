e20b4dba5897385d32b5b9df32cbb58b2621e4a7
andreibardas <bardas.andrei@yahoo.com>
2020-07-10 16:13:00 +0300

    legatura intre front si back plus axios pe front

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import axios from 'axios';
 
 import SHOP_DATA from "./shop.data";
 import "../../components/collection-preview/collection-preview.component";
 import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+import { compose } from "redux";
+// import product from "../../../../Backend/validation/product";
 
 
 
@@ -13,18 +16,56 @@ class ShopPage extends React.Component{
         super(props);
 
         this.state = {
-            collections: SHOP_DATA
+            collections: SHOP_DATA,
+            products: []
         };
     }
-        render(){
-                const {collections} = this.state;
-                return (<div className="shop-page">
+
+    componentDidMount(){
+        axios.get('/api/products')
+            .then(res => {
+                this.setState({ products: res.data });
+                this.state.products.map(product => console.log(product));
+            })
+            .catch(err => {console.log(err)})
+    }
+
+    render(){
+                const {collections} = this.state.collections;
+                return (
+                <div className="shop-page">
+
+
                     {
+                    this.state.products.map(product => 
+                        <div style={{marginTop: "100px"}}>
+                        {
+                            <h1 style={{textDecoration: "underline"}}>{product.name}</h1>
+                        }
+                        {
+                            <h2>Pret: {product.price} lei</h2>
+                        }
+                        {
+                            <h2>Descriere: {product.description}</h2>
+                        }
+                        {
+                            <h2>Categorie: {product.category}</h2>
+                        }
+                        </div>
+                        )    
+                    
+                    }
+
+
+
+
+                    {/* {
                         collections.map(({id, title, routeName, items}) => (
                             <CollectionPreview key={id} title={title} routeName={routeName} items={items}/>
                         ))
-                    }
-                </div>);
+                    } */}
+                </div>
+                );
 
         }
     }
